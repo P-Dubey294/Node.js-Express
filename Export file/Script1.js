8a2b1cec30f9b45b4f23b01ec8ed42d04c5d8dfe
P-Dubey294 <dpratham649@gmail.com>
2025-05-10 13:23:10 +0530

    Exporting and Accessing the files

---
+++ Export file/Script1.js
@@ -0,0 +1,5 @@
+const math = require("./Math.js")
+console.log(math.sum(5,4));
+console.log(math.mul(5,4));
+console.log(math.g);
+console.log(math.PI);
---
+++ Script.js
@@ -0,0 +1,11 @@
+//  let n=5;
+
+//  for(let i=1 ; i<=n; i++){
+//     console.log("Hello India", i);
+//  }
+//  console.log("PrathamKumar Dubey");
+
+let args = process.argv;
+for(let i=2; i<args.length; i++){
+   console.log("hello" , args[i]);
+}
