de4879ca76c14c898c1185ebe062ee7701b0325c
P-Dubey294 <dpratham649@gmail.com>
2025-05-13 10:22:41 +0530

    request & response handling:

--- Express.js/index.js
+++ Express.js/index.js
@@ -4,5 +4,20 @@ const app = express();
 let port = 5000;
 
 app.listen (port, () => {
-    console.log(`app listen is sucessfully on port ${port}`);
+    console.log(`app listen is sucessfully on port ${port}`); // Server created 
+});
+
+app.use((req,res) => {
+    console.log(req); // sending Request
+    console.log("new Incomiing request Recived Successfully!");  // sending the response 
+    res.send(" Thankyou for giving to me a opprtunity for indroduced my self") // sending the respoonse in a string
+    res.send({
+        name : "Prathamkumar Dubey",
+        college : "GH raisoni college of engeenering and management",
+        city : "Nagpure",
+        contactNo : 9529754950,
+        Branch : "MAC" 
+    });  // sending response in object formate.
+
+    res.send("<h1>Prathamkumar Dubey<h1>") // sending response in HTML tag.
 });
