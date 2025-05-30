dc7c4bb43445505ba5bd06fa38584e9d790ee85e
P-Dubey294 <dpratham649@gmail.com>
2025-05-30 16:19:14 +0530

    Fix input name in post request form and enhance response messages

--- MISCLLENIOUS/Backend/index.js
+++ MISCLLENIOUS/Backend/index.js
@@ -2,13 +2,17 @@ import express from "express"
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({extended : true})); // middlewqare
+app.use(express.json());
+
 app.get("/register", (req,res) => {
     let {user, password} = req.query;
     res.send(`Standard Get Response Welcome ${user} !`)
 });
 
 app.post("/register", (req,res) => {
-    res.send("Standard Post Response")
+    let {user, password} = req.body;
+    res.send(`Standard Post Response Welcome ${user}!`)
 }); 
 
 app.listen(port, () => {
