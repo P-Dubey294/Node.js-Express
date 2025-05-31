fdd258dae9be2cd0da4b749bad79cd45e7ef41c9
P-Dubey294 <dpratham649@gmail.com>
2025-05-31 11:33:40 +0530

    Prototype

---
+++ MISCLLENIOUS/Frontend/app.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3,4,5,6,7,8]
+let arr2 = [5,6,7,8,9,0];
+arr.sayHello = () => {
+    console.log(" hello. iam arry");
+}
+
+arr2.sayHello = () => {
+    console.log(" hello. iam arry");
+}
